1badd38dd270bc32cfc6e09f3edb75226e3dd603
archit mehta <pathashala06@gmail.com>
2019-10-13 13:36:38 +0530

    Data loading code updated Added data file

--- load_and_filter_data.py
+++ load_and_filter_data.py
@@ -1,25 +1,26 @@
 import csv
 import pymysql
-
+import os
+    
 # global variable
-dataset_file_name = "/home/archit/Documents/STUDY/CCE_IISC/BasicsofDataAnalytics/delhi-weather-data_test.csv"
-
-
+dataset_file_name = "/home/archit/Documents/STUDY/CCE_IISC/BasicsofDataAnalytics/dataset/delhi_weather_test.csv"
 db = pymysql.connect("localhost", "root", "toortoor", "analytics")
 cursor = db.cursor()
+dataset_name = os.path.splitext(os.path.basename(dataset_file_name))[0]
+
 
 class Wheather:
 
     def __init__(self):
         return
 
-    def load_csv_file(self,filename):
-        with open(filename , 'r') as f:
+    def load_csv_file(self, filename):
+        with open(filename, 'r') as f:
             reader = csv.reader(f)
             temp_list = list(reader)
         return temp_list
 
-    def filter_data(self,temp_data):
+    def filter_data(self, temp_data):
         length = len(temp_data)
         i = 0
         while i < length:
@@ -28,48 +29,48 @@ class Wheather:
             i += 1
         return temp_data
 
-    def create_structured_data(self,row_data):
+    def create_structured_data(self, row_data):
         column_name = row_data[0]
         row_data.pop(0)
-        for i in range(len(row_data)) :
+        for i in range(len(row_data)):
             for j in range(len(row_data[0])):
-                if(row_data[i][j] == ''):
+                if (row_data[i][j] == ''):
                     row_data[i][j] = 'NULL'
         return column_name, row_data
 
-# INSERT INTO delhi_weather (datetime_utc, _conds, _dewptm, _fog, _hail, _heatindexm, _hum, _precipm, _pressurem, _rain, _snow, _tempm, _thunder, _tornado, _vism,	_wdird, _wdire,	_wgustm, _windchillm, _wspdm )
-# VALUES ('1996-11-01 12:00','Smoke',10,0,0,NULL,32,NULL,-9999,0,0,28,0,0,NULL,0,'North',NULL,NULL,NULL);
+    def check_if_exists(self):
+        check_table_str = "show tables like \"" + dataset_name + "\";"
+        table_count = cursor.execute(check_table_str)
+        return table_count
 
-    def insert_data_to_db(self,structured_data,column_name):
-        cnt = 0
-        columns = ",".join(column_name)
+    def create_data_table(self, column_name):
+        columns = " DOUBLE,".join(column_name) + " DOUBLE"
+        create_table_str = "CREATE TABLE IF NOT EXISTS " + dataset_name + "(id int  NOT NULL AUTO_INCREMENT," + columns + ",PRIMARY KEY (id));"
+        print ("create table query : "+create_table_str)
+        cursor.execute(create_table_str)
+
+    def insert_data_to_db(self, structured_data, column_name):
+        count = 0
+        columns = "`" + "`,`".join(column_name) + "`"
         for list in structured_data:
-            # values = str(list)[1:-1
-            value1 = ",".join(list[2:16])
-            value2 = ",".join(list[17:20])
-            temp_str = "INSERT INTO delhi_weather (" + columns + ") VALUES ( '" + list[0] + "','" + list[1] + "'," +value1 +",'"+list[16]+"',"+value2 +") ;"
-            print(temp_str)
+            values = ",".join(list)
+            temp_str = "INSERT INTO `" + dataset_name + "` ( " + columns + ") VALUES (" + values + ") ;"
             cursor.execute(temp_str)
-            cnt = cnt + 1
+            count = count + 1
         cursor.execute("commit")
-        return cnt
+        return count
 
-    # values = ",".join(list)
-    # values = str(list)[1:-1]
+
 
 if __name__ == '__main__':
     my_weather = Wheather()
     row_data = my_weather.load_csv_file(dataset_file_name)
     column_name, structured_data = my_weather.create_structured_data(row_data)
-    # print(structured_data)
+    print("column name " + str(column_name))
+    if (my_weather.check_if_exists()):
+        print("The table already exists. Overwriting content ...")
+    else:
+        my_weather.create_data_table(column_name)
+        print("Table created: " + dataset_name)
     inserted_rows_count = my_weather.insert_data_to_db(structured_data, column_name)
-    print("number of rows inserted " + str(inserted_rows_count))
-    # print(column_name)
-    # data = my_weather.filter_data(data)
-    # print(data)
-
-# db = pymysql.connect("localhost", "root", "toortoor", "analytics")
-# cursor = db.cursor()
-# cursor.execute("SELECT VERSION()")
-# db_version = cursor.fetchone()
-# print(db_version)
+    print("Number of rows inserted :" + str(inserted_rows_count))
