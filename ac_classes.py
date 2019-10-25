239e296ad5d9a29fcaf32c7fd7fe01b0d202d0ad
knn <kn.neelalohitha@gmail.com>
2019-10-25 23:29:07 +0530

    Update ac_classes.py -- Add new method to datamodel
    Signed-off-by: knn <kn.neelalohitha@gmail.com>

--- ac_classes.py
+++ ac_classes.py
@@ -14,13 +14,13 @@ class IndivModel:
     def var(self):
         self.var = st.variance(self.values, self.mean)
         return self.var
-"""
+#"""
 a = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0]
 b = [9.0, 8.0, 9.0, 12.0, 9.0, 12.0, 11.0, 7.0, 13.0, 9.0, 11.0, 10.0]
 
 x = IndivModel(a)
 y = IndivModel(b)
-"""
+#"""
 class DataModel(IndivModel):
 
     def __init__(self,x, y):
@@ -31,23 +31,23 @@ class DataModel(IndivModel):
     #global x_bar, y_bar
     def x_bar(self):
     #    global x_bar
-        self.x_bar = x.mean()
+        self.x_bar = x.mean
      #   x_bar = self.x_bar
         return self.x_bar
         #return self.x_bar
 
     def y_bar(self):
      #   global y_bar
-        self.y_bar = y.mean()
+        self.y_bar = y.mean
       #  y_bar = self.y_bar
         return self.y_bar
 
     def x_var(self):
-        self.x_var = x.var()
+        self.x_var = x.var
         return self.x_var
 
     def y_var(self):
-        self.y_var = y.var()
+        self.y_var = y.var
         return self.y_var
 
     def cov(self):
@@ -59,9 +59,13 @@ class DataModel(IndivModel):
         self.threshold = 1.96/ math.sqrt(len(self.x.values))
         return self.corr_coeff
 
-    #def coeff(self, order):
+    def nlr_coef(self):
+        self.poly_coeff = []
+        self.exp_coeff = []
+        self.sin_coeff = []
         
         
+       
 
 """
     def nlr_coeff(self):
@@ -69,5 +73,5 @@ class DataModel(IndivModel):
     def anova(self):
         ...
 """
-#t = DataModel(x, y)
+t = DataModel(x, y)
 
