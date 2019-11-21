605d47503303d8864c703035a750c9ecc4293da6
Nikhil Budhwani <Nikhil_Budhwani@ultimatesoftware.com>
2019-11-21 06:22:58 +0530

    ANOVA code tested and done as per teh requirement, ANOVA CI only Tm adn TC are done

--- ac_classes.py
+++ ac_classes.py
@@ -71,7 +71,9 @@ class BiDataModel(IndivModel):
         self.sse = None
         self.f = None
         self.p = None
-        
+        self.model_confidence = None
+        self.t_m=None
+        self.t_c=None
         
        
 
@@ -107,6 +109,15 @@ class MultiDataModel():
 
     def linear_regression_coeff(self):
         self.lin_reg_coeff = []
+
+    def anova(self):
+        self.msr = None
+        self.mse = None
+        self.ssr = None
+        self.sse = None
+        self.f = None
+        self.p = None
+        self.model_confidence = None
         
         
         
